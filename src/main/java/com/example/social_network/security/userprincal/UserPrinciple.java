6c91c2760f334e9c44e1fd59dad549a3fd09c5d1
Pham Tuan Hung 3011 <phamtu571990@gmail.com>
2022-03-10 09:55:24 +0700

    crud Post, chưa sửa được kèm theo ảnh

--- src/main/java/com/example/social_network/security/userprincal/UserPrinciple.java
+++ src/main/java/com/example/social_network/security/userprincal/UserPrinciple.java
@@ -63,6 +63,39 @@ public class UserPrinciple implements UserDetails {
                 authorityList
         );
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
@@ -97,4 +130,5 @@ public class UserPrinciple implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
 }
